56223866482564372abd23c95b2a2792e3a84729
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-03-11 15:45:55 +0100

    Feat: add html and js snack-2 + create a let for number sum

---
+++ snack-2/js/script.js
@@ -0,0 +1,6 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// Creare la variabile somma di tutti i numeri inseriti dall'utente
+let numberSum = 0;
+
