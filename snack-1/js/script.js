63ad97363887487c38bdebfe0fa7e1f8962a3d79
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-03-11 15:27:01 +0100

    Feat: add file html and css + create a array void and a cycle for

---
+++ snack-1/js/script.js
@@ -0,0 +1,15 @@
+// Chiedere all'utente 5 numeri. 
+// Inserire i numeri nell'array ma senza creare duplicati 
+// (inserire il numero nell'array solo se non è già presente).
+
+const numberList = [];
+
+for (let i=0; i<5; i++) {
+
+    // Chiedere all'utente 5 Numeri
+    const userNumber = prompt('Dammi un numero');
+    console.log(userNumber)
+
+
+
+}
