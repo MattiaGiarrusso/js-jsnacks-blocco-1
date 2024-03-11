df79e95d1e77cbd1bc4e7cfc7322956b539023c2
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-03-11 17:23:51 +0100

    Feat: add html and js

---
+++ snack-5/js/script.js
@@ -0,0 +1,8 @@
+// Dato un array contenente 10 numeri, 
+// stampare in console il numero più grande.
+
+// Creare l'array di 10 numeri 
+const numberList = [2,13,8,5,11,7,9,1,22,6];
+
+// stampare in console il numero più grande
+console.log(Math.max(...numberList));
